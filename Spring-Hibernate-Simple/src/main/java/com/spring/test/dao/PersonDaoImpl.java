24844837f1508b66632d135e2ed366146df69975
Ally Jarrett <ajarrett@redhat.com>
2014-08-18 19:01:15 +0100

    New Basic Spring / Hibernate / HSQLDB (in memory) DB Example

---
+++ Spring-Hibernate-Simple/src/main/java/com/spring/test/dao/PersonDaoImpl.java
@@ -0,0 +1,43 @@
+package com.spring.test.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.spring.test.model.Person;
+
+/**
+ * DAO implementation for Person entity
+ * 
+ * @author DevCrumb.com
+ */
+@Transactional(propagation = Propagation.REQUIRED)
+public class PersonDaoImpl {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public int save(Person person) {
+		em.persist(person);
+		return person.getId();
+	}
+
+	public List<Person> getAll() {
+		return em.createQuery("SELECT p FROM Person p", Person.class)
+				.getResultList();
+	}
+	
+	public Person retrievePerson(int idPerson) {
+		return em.find(Person.class, idPerson);
+	}
+	
+	public int deletePerson(Person person) {
+		em.remove(em.contains(person) ? person : em.merge(person));
+		 return person.getId();
+	}
+
+}
---
+++ Spring-Hibernate-Simple/src/test/java/com/spring/test/db/DBTest.java
@@ -0,0 +1,74 @@
+package com.spring.test.db;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.test.dao.PersonDaoImpl;
+import com.spring.test.model.Person;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class DBTest {
+
+    /**
+     * Not a great test as I am using test ordering but it's simple and to the point...
+     */
+	
+	@Autowired
+	private PersonDaoImpl personDao;
+	
+	private static Person person1;
+	private static Person person2;
+	
+	@BeforeClass
+	public static void initPerson(){
+		person1 = new Person(1, "Joe", "New York");
+        person2 = new Person(2, "James", "London");
+	}
+	
+	@Test
+	public void testASavePerson() {
+
+        personDao.save(person1);
+        personDao.save(person2);       
+        assertTrue(personDao.getAll().size() == 2);
+		
+	}
+	
+	@Test
+	public void testDeletePerson(){
+		
+		personDao.deletePerson(person1);
+		
+		List<Person> getPeople = personDao.getAll();
+		assertTrue(getPeople.size() == 1);
+		
+		for (Person person : getPeople ) {
+			assertTrue(person.getName().equals("James"));
+		}
+		
+	}
+	
+	@Test
+	public void testBRetrievePerson(){
+		
+		Person newPerson = personDao.retrievePerson(2);
+		assertTrue(newPerson.getId() == 2);
+		assertTrue(newPerson.getName().equals("James"));
+		
+	}
+
+
+}
