6e92de44a1d02fd69a06d6808a519f119dd49320
Ally Jarrett <ajarrett@redhat.com>
2014-08-18 20:52:54 +0100

    Updated c3p0 config and migrated to hibernate-c3p0 dependency

--- Spring-Hibernate-Simple/src/test/java/com/spring/test/db/DBTest.java
+++ Spring-Hibernate-Simple/src/test/java/com/spring/test/db/DBTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
